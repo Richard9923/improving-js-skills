2dc18dd9bf4b4297dcb88231b5304039a70f30d3
Richard <programmingislife23@gmail.com>
2024-06-23 16:17:19 -0300

    add

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 const elemento = document.getElementById("h2");
+const p = document.getElementsByTagName("p");
+const titulos = (document.getElementsByClassName("titulo").innerHTML = "helou");
+const xstyke = document.querySelectorAll("p.intro");
