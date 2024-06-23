bea789391637d542ef5a3007b3489d9fa333c6a1
Richard <programmingislife23@gmail.com>
2024-06-23 19:12:49 -0300

    praticando

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const elemento = document.getElementById("h2");
 const p = document.getElementsByTagName("p");
 const titulos = (document.getElementsByClassName("titulo").innerHTML = "helou");
 const xstyke = document.querySelectorAll("p.intro");
+const teste = document.getElementsByClassName("ola");
+const testeID = document.getElementById("teste");
+
+teste.innerHTML = "TITULO";
+testeID.innerHTML = "titulo";
