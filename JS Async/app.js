1dd74ed86388d06f5a9e76a5436f3bdf671adffe
Richard <programmingislife23@gmail.com>
2024-06-24 10:22:54 -0300

    removendo numeros negativos da array

--- JS Async/app.js
+++ JS Async/app.js
@@ -34,3 +34,17 @@ let result = myCalculator(2, 5);
 mySecondDisplay(result);
 
 mySecondCalculator(5, 7, mySecondDisplay);
+
+const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+const postNumbers = removeNeg(myNumbers, (x) => x >= 0);
+paragrafo2.innerHTML = postNumbers;
+
+function removeNeg(numbers, CallBack) {
+  const myArray = [];
+  for (const x of numbers) {
+    if (CallBack(x)) {
+      myArray.push(x);
+    }
+  }
+  return myArray;
+}
