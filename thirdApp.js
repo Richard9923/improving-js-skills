05071ba0c360d32f2d842caac19464b607fa20c5
Richard <programmingislife23@gmail.com>
2024-06-24 09:39:32 -0300

    creando elementos

--- thirdApp.js
+++ thirdApp.js
@@ -1,5 +1,14 @@
 const creating1 = document.createElement("p");
 const node = document.createTextNode("This is new");
 const element = document.getElementById("div1");
+const child = document.getElementById("p1");
+const child2 = document.getElementById("p2");
+const creating2 = document.createElement("h2");
+const childCreating2 = document.createTextNode("Keep going");
+
 creating1.appendChild(node);
 element.appendChild(creating1);
+creating2.appendChild(childCreating2);
+
+element.replaceChild(creating1, child);
+element.replaceChild(creating2, child2);
