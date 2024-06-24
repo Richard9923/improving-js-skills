58f40adbb909d81ba8d96f49f04fe2294eb32480
Richard <programmingislife23@gmail.com>
2024-06-24 10:16:14 -0300

    keeping improving

---
+++ JS Async/app.js
@@ -0,0 +1,36 @@
+const paragrafo = document.getElementById("demo");
+const paragrafo2 = document.getElementById("demo2");
+
+function mySecondDisplay(some) {
+  paragrafo2.innerHTML = some;
+}
+
+function myCalculator(a, b) {
+  let calc = a * b;
+  return calc;
+}
+
+function mySecondCalculator(num1, num2, myCallBack) {
+  let sum = num1 + num2;
+  myCallBack(sum);
+}
+
+function myDisplayer(some) {
+  paragrafo.innerHTML = some;
+}
+
+function myFirst() {
+  myDisplayer("Hello");
+}
+
+function mySecond() {
+  myDisplayer("Goodbye");
+}
+
+myFirst();
+mySecond();
+
+let result = myCalculator(2, 5);
+mySecondDisplay(result);
+
+mySecondCalculator(5, 7, mySecondDisplay);
