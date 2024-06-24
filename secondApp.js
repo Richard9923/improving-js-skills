fcd61fdca517d2cf5cd80a3d35c8993192a009c3
Richard <programmingislife23@gmail.com>
2024-06-24 08:44:33 -0300

    function com calculo ao click

--- secondApp.js
+++ secondApp.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById("btn");
+const testando = document.getElementById("teste");
 
 function displayDate() {
   document.getElementById("demo").innerHTML = Date();
@@ -15,6 +16,15 @@ btn.addEventListener("click", mySecondFunction);
 btn.addEventListener("mouseover", myFunction);
 btn.addEventListener("mouseout", mySecondFunction);
 
+let p1 = 2;
+let p2 = 5;
+testando.addEventListener("click", function () {
+  myThirdFunction(p1, p2);
+});
+function myThirdFunction(a, b) {
+  testando.innerHTML = a * b;
+}
+
 function myFunction() {
   alert("Here we go again.");
 }
@@ -22,3 +32,7 @@ function myFunction() {
 function mySecondFunction() {
   alert("Here we go a third time hahaha");
 }
+
+window.addEventListener("resize", function () {
+  this.document.getElementById("demo").innerHTML = "funcionou";
+});
