5d392f077b82064f1d47e5125827281d426eb9a9
Richard <programmingislife23@gmail.com>
2024-06-24 09:54:02 -0300

    let's go

--- thirdApp.js
+++ thirdApp.js
@@ -6,11 +6,19 @@ const child2 = document.getElementById("p2");
 const creating2 = document.createElement("h2");
 const childCreating2 = document.createTextNode("Keep going");
 const myCollection = document.getElementsByTagName("p");
+const myNodeList = document.querySelectorAll("p");
+
+console.log(myNodeList);
+console.log(myNodeList[1]);
+console.log(myNodeList.length);
 
 for (let i = 0; i < myCollection.length; i++) {
   myCollection[i].style.color = "green";
 }
 
+for (let i = 0; i < myNodeList.length; i++) {
+  myNodeList[i].style.color = "blue";
+}
 creating1.appendChild(node);
 element.appendChild(creating1);
 creating2.appendChild(childCreating2);
