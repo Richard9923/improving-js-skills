25d59db1b2a2951a5cc776660bbde88af684071b
Richard <programmingislife23@gmail.com>
2024-06-30 13:09:56 -0300

    aprendendo sobre comunicacão de input user com servidor

--- JS Ajax/ajax.js
+++ JS Ajax/ajax.js
@@ -1,7 +1,17 @@
+// function loadDoc() {
+//   const request = new XMLHttpRequest();
+//   request.onload = function () {
+//     document.getElementById("demo").innerHTML = this.getAllResponseHeaders();
+//   };
+//   request.open("GET", "ajax_info_txt");
+//   request.send();
+// }
+
 function loadDoc() {
   const request = new XMLHttpRequest();
   request.onload = function () {
-    document.getElementById("demo").innerHTML = this.getAllResponseHeaders();
+    document.getElementById("demo").innerHTML =
+      this.getResponseHeader("Last-Modified");
   };
   request.open("GET", "ajax_info_txt");
   request.send();
