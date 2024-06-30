bab30d217240818aecf2d77a0845dde5fe9e07d3
Richard <programmingislife23@gmail.com>
2024-06-30 09:08:46 -0300

    localizacao

---
+++ JS WebApi/app.js
@@ -0,0 +1,17 @@
+const myElement = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    myElement.innerHTML = "Geolocalização não é suportada nesse navegador.";
+  }
+}
+
+function showPosition(position) {
+  myElement.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+}
