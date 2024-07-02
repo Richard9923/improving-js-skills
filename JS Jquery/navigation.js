274de5680cfc3dc0ca64318f71997c6e7fde8ca9
Richard <programmingislife23@gmail.com>
2024-07-02 18:19:28 -0300

    menu animação

--- JS Jquery/navigation.js
+++ JS Jquery/navigation.js
@@ -6,4 +6,22 @@ $(document).ready(function () {
   $(".info-title").click(function () {
     $(".info-content").slideToggle("fast");
   });
+
+  $(document).ready(function () {
+    $(".content-box").click(function () {
+      $(".content-box").animate(
+        {
+          width: "500px",
+          height: "+=100px",
+        },
+        "slow"
+      );
+      $(".content-after").show().animate(
+        {
+          opacity: "1",
+        },
+        3000
+      );
+    });
+  });
 });
