668b9b227cb6366a1eaaf7dac531a07a5ef1d56a
Richard <programmingislife23@gmail.com>
2024-07-02 16:18:51 -0300

    navigation fading learning

--- JS Jquery/main.js
+++ JS Jquery/main.js
@@ -1,3 +1,34 @@
-$("#test").click(function () {
-  alert("clicked");
+$(document).ready(function () {
+  $("#test").click(function () {
+    $(this).hide();
+  });
+});
+
+$("#p1").hover(function () {
+  $(this).hide();
+});
+
+$("#p2").click(function () {
+  $(this).hide();
+});
+
+$("#p3").dblclick(function () {
+  $(this).hide();
+});
+
+$("#p4").mouseout(function () {
+  $(this).hide();
+});
+
+let menuTrigger = false;
+$(document).keypress(function (e) {
+  if (e.which == 13) {
+    if (menuTrigger === false) {
+      $("nav").fadeIn(500);
+      menuTrigger = true;
+    } else if (menuTrigger === true) {
+      $("nav").fadeOut(500);
+      menuTrigger = false;
+    }
+  }
 });
