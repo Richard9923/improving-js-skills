8bcdf64c71a1774cf97e1ce42b07c1e373ba6128
Richard <programmingislife23@gmail.com>
2024-07-02 18:24:41 -0300

    aprendendo mais efeitos com jquery

---
+++ JS Jquery/maisEfeitos/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(".content-box").click(function () {
+    $(".content-box").animate(
+      {
+        width: "500px",
+        height: "400px",
+      },
+      3000
+    );
+    $(".content-box").animate(
+      {
+        fontSize: "30px",
+      },
+      3000
+    );
+  });
+});
