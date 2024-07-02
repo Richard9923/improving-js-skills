9c260515e7cff0ba447df613331b7d79ae6a8db7
Richard <programmingislife23@gmail.com>
2024-07-02 19:44:16 -0300

    Pegando informações utilizando Jquery dom manipultation

---
+++ JS Jquery/app5.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#btn1").click(function () {
+        $("#test1").text("This is the new text!");
+    })
+    $("#btn2").click(function () {
+        $("#test2").html("<i>This is the new text</i>");
+    })
+    $("#btn3").click(function () {
+        $("#test3").val("New input data!");
+    })    
+})
